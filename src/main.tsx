295be372384f04e4644bcc71809483b6849fa5be
Steve Warsa <swarsa480@gmail.com>
2025-06-25 04:51:38 -0700

    Fixed some routing issues when the app is deployed in production

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import { Provider } from 'react-redux'
-import { ModuleRegistry, AllCommunityModule } from 'ag-grid-community'
+import {StrictMode} from 'react'
+import {createRoot} from 'react-dom/client'
+import {BrowserRouter} from 'react-router-dom'
+import {Provider} from 'react-redux'
+import {ModuleRegistry, AllCommunityModule} from 'ag-grid-community'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './index.css'
 import App from './App.tsx'
@@ -11,12 +11,14 @@ import store from './store/index.ts'
 // Register AG Grid modules
 ModuleRegistry.registerModules([AllCommunityModule])
 
+const basename = import.meta.env.DEV ? '' : '/weight-tracker';
+
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </Provider>
-  </StrictMode>,
+    <StrictMode>
+        <Provider store={store}>
+            <BrowserRouter basename={basename}>
+                <App/>
+            </BrowserRouter>
+        </Provider>
+    </StrictMode>,
 )
